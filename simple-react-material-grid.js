c5038c800fa312a9ad697354166b91ad15925dae
Paul Stead <paul.stead@tyke.co.uk>
2017-10-16 21:51:47 +0100

    draw basic grid

--- simple-react-material-grid.js
+++ simple-react-material-grid.js
@@ -18,11 +18,13 @@ class SimpleReactMaterialGrid extends Component {
     }
 
     updateCanvas() {
+
         const ctx = this.refs.canvas.getContext('2d');
-        ctx.clearRect(0, 0, 300, 300);
         // draw children “components”
-        rect({ctx, x: 10, y: 10, width: 50, height: 50});
-        rect({ctx, x: 110, y: 110, width: 50, height: 50});
+        // rect({ctx, x: 10, y: 10, width: 50, height: 50});
+        // rect({ctx, x: 110, y: 110, width: 50, height: 50});
+
+        buildGrid(ctx, 300, 300, 5, "grey", 2);
     }
 
 
@@ -39,6 +41,53 @@ class SimpleReactMaterialGrid extends Component {
 function rect(props) {
     const {ctx, x, y, width, height} = props;
     ctx.fillRect(x, y, width, height);
+}
+
+function buildGrid(ctx, width, height, gridPixelSize, color, gap) {
+
+
+    ctx.clearRect(0, 0, width, height);
+
+    // var canvas = $('#' + div + '').get(0);
+    // var ctx = canvas.getContext("2d");
+    //ctx.fillStyle="rgba(255, 255, 255, 0)";
+    //ctx.fillRect(0, 0, 500, 300);
+    ctx.lineWidth = 0.5;
+    ctx.strokeStyle = color;
+
+    // horizontal grid lines
+    for (var i = 0; i <= height; i = i + gridPixelSize) {
+        ctx.beginPath();
+        ctx.moveTo(0, i);
+        ctx.lineTo(width, i);
+        if (i % parseInt(gap) == 0) {
+            ctx.lineWidth = 0.5;
+        } else {
+            ctx.lineWidth = 0.5;
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    // vertical grid lines
+    for (var j = 0; j <= width; j = j + gridPixelSize) {
+        ctx.beginPath();
+        ctx.moveTo(j, 0);
+        ctx.lineTo(j, height);
+        if (j % parseInt(gap) == 0) {
+            ctx.lineWidth = 0.5;
+        } else {
+            ctx.lineWidth = 0.5;
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    for(var ii = 0; ii <= width; ii+=2) {
+        for(var jj=0; jj <= height; jj+=2) {
+            ctx.clearRect(ii,jj,1,1);
+        }
+    }
 }
 
 export default SimpleReactMaterialGrid;
